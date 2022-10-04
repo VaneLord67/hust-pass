2c75465364147bd3d6b21219caa901ba6465006f
CJR <524429470@qq.com>
2022-10-04 16:14:59 +0800

    feat: 代码结构调整；Docker部署

--- spider/spider.go
+++ spider/spider.go
@@ -2,6 +2,7 @@ package spider
 
 import (
 	"fmt"
+	"github.com/tebeka/selenium/chrome"
 	"hust-pass/config"
 	"hust-pass/ocr"
 	"log"
@@ -48,7 +49,7 @@ func LoginGetElec(wd selenium.WebDriver) (string, error) {
 				return false, nil
 			}
 			return elecResult != "", nil
-		}, time.Second*5)
+		}, time.Second*30)
 		if err != nil {
 			continue
 		}
@@ -66,7 +67,7 @@ func Login(wd selenium.WebDriver, ocrResult string) error {
 			return true, nil
 		}
 		return false, nil
-	}, time.Second*5)
+	}, time.Second*30)
 	if err != nil {
 		return err
 	}
@@ -121,4 +122,32 @@ func GetJSIDAndIPPool(wd selenium.WebDriver) (string, string, error) {
 		}
 	}
 	return jsid, ipPool, nil
+}
+
+func InitWebDriver() (selenium.WebDriver, error) {
+	//连接到本地运行的 WebDriver 实例
+	//这里的map键值只能为browserName，源码里需要获取这个键的值，来确定连接的是哪个浏览器
+	caps := selenium.Capabilities{"browserName": "chrome"}
+	//禁止图片加载，加快渲染速度
+	imagCaps := map[string]interface{}{
+		"profile.managed_default_content_settings.images": 2,
+	}
+	//设置实验谷歌浏览器驱动的参数
+	chromeCaps := chrome.Capabilities{
+		Prefs: imagCaps,
+		Args: []string{
+			"--headless", //设置Chrome无头模式
+		},
+	}
+	//添加浏览器设置参数
+	caps.AddChrome(chromeCaps)
+	//NewRemote 创建新的远程客户端，这也将启动一个新会话。 urlPrefix 是 Selenium 服务器的 URL，必须以协议 (http, https, ...) 为前缀。
+	//为urlPrefix提供空字符串会导致使用 DefaultURLPrefix,默认访问4444端口，
+	//所以最好自定义，避免端口已经被抢占。后面的路由还是照旧DefaultURLPrefix写
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub",
+		config.GlobalConfig.ChromeDriverServicePort))
+	if err != nil {
+		return nil, err
+	}
+	return wd, nil
 }
